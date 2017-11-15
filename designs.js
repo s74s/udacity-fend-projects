5ef2994169b8091a80ec200e35530bca620b39bf
LWebGH <sushkovstas@gmail.com>
2017-11-16 01:36:23 +0300

    Colorpicker, painting()

--- designs.js
+++ designs.js
@@ -6,30 +6,49 @@ $(() => {
   const listShow = $('.list-show-btn');
   const toDoList = $('.to-do-list');
   const toDoItem = $('.to-do-item');
-  const removeBtn = $('.remove-btn');
+  const hideBtn = $('.hide-btn');
+  const warningText = $('.grid-generator p');
   // let height = $('#input_height').val();
   // let width = $('#input_height').val();
-  let colorPicker = $('#colorPicker').val();
-
-  console.log(pixelTable, createBtn, colorPicker);
-
-
+  let color = $('.colorPicker').val();
+  const colorArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+  const rdmColor = () => {
+    let color = '#';
+    while (color.length < 7) {
+      color += colorArray[Math.round(Math.random() * 15)];
+    }
+    return color;
+  };
   $(btnPanel).hide();
+  $('.palette').hide();
+  $('.color-picker').hide();
+
   function makeGrid() {
     console.log('Making grid');
     $('#pixel_canvas').remove();
-    $('.btn-panel').after('<table id=pixel_canvas></table>');
+    $('.control-panel').after('<table id="pixel_canvas"></table>');
     let w = $('#input_width').val();
     let h = $('#input_height').val();
-    if (w > 60) w = 60;
-    if (h > 40) h = 40;
+    if (w > 40) {
+      w = 40;
+      $(warningText).css('opacity', '1');
+    }
+    if (h > 40) {
+      h = 40;
+      $(warningText).css('opacity', '1');
+    }
     for (let row = 1; row <= h; row += 1) {
       $('#pixel_canvas').append(`<tr id="${row}"></tr>`);
       for (let columns = 1; columns <= w; columns += 1) {
-        $(`#${row}`).append(`<td id="${row}-${columns}"></td>`);
+        $(`#${row}`).append('<td></td>');
       }
     }
-    $(btnPanel).fadeIn(500);
+    $('#pixel_canvas').on('mousedown', 'td', (evt) => {
+      $(evt.target).css('background-color', color);
+    });
+    $('.palette').fadeIn(2500);
+    $('.color-picker').fadeIn(2500);
+    $(btnPanel).fadeIn(2500);
   }
 
   $(createBtn).on('click', () => {
@@ -41,27 +60,42 @@ $(() => {
     $(listShow).toggleClass('list-show-btn--closed');
   });
 
-  // $('#pixel_canvas').on('click', 'td', (evt) => {
-  //   console.log(evt.target);
-  //   $(evt.target).css('background-color', 'red');
-  // });
-
   $(toDoItem).on('click', (evt) => {
     $(evt.currentTarget).toggleClass('success');
   });
 
-  $(removeBtn).on('click', () => {
-    $(btnPanel).fadeOut(500);
-    $('#pixel_canvas').fadeOut(500);
-    setTimeout(() => {
-      $('#pixel_canvas').remove();      
-    }, 500);
-    $('.btn-panel').after('<table id=pixel_canvas></table>');
+  $(hideBtn).on('click', () => {
+    if (hideBtn.text() == 'Hide') {
+      $('#pixel_canvas').hide(300);
+      $(hideBtn).text('Show');
+    } else {
+      $('#pixel_canvas').show(300);
+      $(hideBtn).text('Hide');
+    }
   });
-});
 
-// const toDoList = document.querySelector('.to-do-list');
-// toDoList.addEventListener('click', (evt) => {
-//   console.log(evt.target);
-// });
+  $('.fill-btn').on('click', () => {
+    $('#pixel_canvas').find('td').css('background-color', color);
+  });
+
+  $('.clear-btn').on('click', () => {
+    $('#pixel_canvas').find('td').css('background-color', 'white');
+  });
+
+  $('.palette').on('click', '.palette-btn', (evt) => {
+    color = $(evt.target).css('background-color');
+    $('.fill-btn').css('background-color', color);
+  });
+
+  $('.colorPicker').change(() => {
+    color = $('.colorPicker').val();
+    $('.fill-btn').css('background-color', color);
+  });
+
+  $('.random-color').on('click', () => {
+    color = rdmColor();
+    $('.fill-btn').css('background-color', color);
+  });
+
+});
 
