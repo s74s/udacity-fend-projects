60ccbe80f5566d762b1b866447cea0010d1dcf98
LWebGH <sushkovstas@gmail.com>
2018-02-18 01:34:27 +0300

    Left only to complete - images

---
+++ portfolio-site/js/index.js
@@ -0,0 +1,8 @@
+let tl = new TimelineMax();
+    
+tl
+  .fromTo('.main-header__left', 1.5, { x:-300, opacity: 0 }, { x: 0, opacity: 1})
+  .staggerFromTo('.main-nav a', 1.5, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, 0.25)
+  .staggerFromTo('.to-animate', 0.3, { y: 20, opacity: 0 }, { y: 0, opacity: 1 }, 0.2)
+  .fromTo('.hero p', 2, { x: 300, opacity: 0 }, { x: 0, opacity: 1 }, "-= 1.5")
+  .staggerFromTo('.project__preview', 0.5, { opacity: 0, y: 30 }, { opacity: 1, y: 0 }, 0.25) 
